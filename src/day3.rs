9e20ad41818284b34270aee6ff160aeced56f984
Eric Xiao <ericxiao0000@gmail.com>
2022-12-15 19:12:29 -0800

    improved day 3

--- src/day3.rs
+++ src/day3.rs
@@ -7,7 +7,7 @@ fn get_item_value(c: char) -> u64 {
 }
 
 fn part1() {
-    let input = include_str!("../input/day3.example");
+    let input = include_str!("../input/day3.input");
     let mut priority_sum = 0;
 
     for rucksack in input.lines() {
@@ -19,7 +19,6 @@ fn part1() {
 
         for item in rucksack.chars().skip(compartment_len) {
             let item_value = get_item_value(item);
-            // println!("{}", item_value);
             if items & (1 << item_value) != 0 {
                 priority_sum += item_value;
                 break;
@@ -34,30 +33,24 @@ fn part2() {
     let input = include_str!("../input/day3.input");
     let mut priority_sum = 0;
 
-    let mut lines = input.lines();
+    let mut rucksacks = input.lines();
 
     loop {
-        let rucksack1 = lines.next().unwrap();
-        let rucksack2 = lines.next().unwrap();
-        let rucksack3 = lines.next().unwrap();
-
-        let mut items1: u64 = 0;
-        for item in rucksack1.chars() {
-            items1 |= 1 << get_item_value(item);
-        }
-        let mut items2: u64 = 0;
-        for item in rucksack2.chars() {
-            items2 |= 1 << get_item_value(item);
-        }
-        let mut items3: u64 = 0;
-        for item in rucksack3.chars() {
-            items3 |= 1 << get_item_value(item);
+        let mut common_item = u64::MAX;
+        for _ in 0..3 {
+            let rucksack = rucksacks.next();
+            if rucksack.is_none() {
+                println!("PRIORITY SUM: {}", priority_sum);
+                return;
+            }
+            let mut items: u64 = 0;
+            for item in rucksack.unwrap().chars() {
+                items |= 1 << get_item_value(item);
+            }
+            common_item &= items;
         }
 
-        let final_item = items1 & items2 & items3;
-        priority_sum += f32::log2(final_item as f32) as i32;
-        println!("PRIORITY SUM: {}", priority_sum);
-
+        priority_sum += f32::log2(common_item as f32) as i32;
     }
 }
 
